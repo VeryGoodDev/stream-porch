76b9f5eb00c0fe153f55768b17d5118c4adbff57
VeryGoodDev <verygooddev@outlook.com>
2020-10-12 01:27:00 -0600

    Made it work as a PWA

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get(`/`, (req, res) => {
 app.get(`/porch-handler`, (req, res) => {
   res.sendFile(`${__dirname}/views/stream-porch-handler.html`)
 })
+app.get(`/service-worker.js`, (req, res) => {
+  res.set(`Content-Type`, `application/javascript`).sendFile(`${__dirname}/service-worker.js`)
+})
+app.get(`/manifest.json`, (req, res) => {
+  res.set(`Content-Type`, `application/json`).sendFile(`${__dirname}/manifest.json`)
+})
 const server = require(`http`).createServer(app)
 
 const options = {
---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener(`install`, evt => {
+  evt.waitUntil(
+    caches.open(`stream-porch`).then(cache => {
+      return cache.addAll([`/`])
+    })
+  )
+})
+
+self.addEventListener(`fetch`, evt => {
+  console.log(evt)
+})
